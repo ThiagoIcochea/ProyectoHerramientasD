5f164330ebdd671bf4de258a633d7b806152bc6b
Thiago <U22330428@utp.edu.pe>
2025-10-10 02:03:32 -0500

    Commit limpio sin secretos

---
+++ backend/obtener_administradores.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+include("conexion.php"); 
+
+
+$sql = "SELECT id_admin, nombre_admin, email_admin, password , fecha_creacion FROM administradores";
+$result = $conn->query($sql);
+
+if (!$result) {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Error al ejecutar la consulta: " . $conn->error
+    ]);
+    exit;
+}
+
+if ($result->num_rows > 0) {
+    $administradores = [];
+    while ($row = $result->fetch_assoc()) {
+        $administradores[] = $row;
+    }
+
+    echo json_encode([
+        "status" => "success",
+        "data" => $administradores
+    ], JSON_UNESCAPED_UNICODE);
+} else {
+    echo json_encode([
+        "status" => "empty",
+        "message" => "No hay administradores registrados."
+    ]);
+}
+
+$conn->close();
+?>
